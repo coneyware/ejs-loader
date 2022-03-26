8d414ab211f98933bca7cee170dd576f2e5d47c4
keisuke konishi <kkoni@coneyware.co.jp>
2022-03-26 15:26:58 +0900

    package & main file add

---
+++ ejs-loader.cjs
@@ -0,0 +1,51 @@
+/**
+ * ejsをwebpackで読み込むためのloader  
+ * 見つけられるloaderではちゃんとinclude対応できないので、作った
+ * @module ejs-loader
+ */
+// import ejs from "ejs";
+const ejs = require("ejs");
+const path = require("path");
+
+const loaderUtils = require("loader-utils");
+
+const cwd = process.cwd();
+
+const mainAsync = async(content, resourcePath, options, callback) => {
+	process.chdir(path.dirname(resourcePath));
+	let ret = null;
+	try {
+		ret = await ejs.render(content, options?.ejsData, options.renderOptions);
+	} catch (err) {
+		callback(err, null);
+	} finally {
+		process.chdir(options.cwd ?? cwd);
+	}
+	callback(null, ret);
+};
+
+const mainSync = (content, resourcePath, options) => {
+	process.chdir(path.dirname(resourcePath));
+	const ret = ejs.render(content, options?.ejsData, options.renderOptions);
+	process.chdir(options.cwd ?? cwd);
+	return ret;
+};
+
+module.exports = function ejsLoader(content, map, meta) {
+	let ret = null;
+	const options = loaderUtils.getOptions(this);
+	if (typeof options.renderOptions === "undefined") {
+		options.renderOptions = {};
+	}
+	options.renderOptions.filename = path.basename(this.resourcePath);
+	if (options?.renderOptions?.async) {
+		// https://webpack.js.org/api/loaders/#asynchronous-loaders
+		const webpackCallback = this.async();
+		mainAsync(content, this.resourcePath, options, (err, result) => {
+			webpackCallback(err, result, map, meta);
+		});
+	} else {
+		ret = mainSync(content, this.resourcePath, options);
+	}
+	return ret;
+};
